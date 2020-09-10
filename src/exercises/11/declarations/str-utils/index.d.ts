f0e34094f4fa218b7dc98125458a54f2bee1d135
Dana <estdanar@gmail.com>
2020-09-10 11:52:35 +0300

    solution

---
+++ src/exercises/11/declarations/str-utils/index.d.ts
@@ -0,0 +1,8 @@
+declare module 'str-utils' {
+    // export const ...
+    export function strReverse(value:string) :string;
+    export function strToLower(value:string) :string;
+    export function strToUpper(value:string) :string;
+    export function strRandomize(value:string) :string;
+    export function strInvertCase(value:string) :string;
+}
